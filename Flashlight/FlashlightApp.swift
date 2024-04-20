57c70b3e2a5db70791c1ea3133e93852193440a0
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-04-20 14:19:49 -0300

    Initial Commit

---
+++ Flashlight/FlashlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashlightApp.swift
+//  Flashlight
+//
+//  Created by Bruno Oliveira on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
